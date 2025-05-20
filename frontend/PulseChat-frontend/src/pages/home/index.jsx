ca03a5269dd1d04233a3bbf7569f07d0f0a9bcde
dikshangurung <dxngrg2058@gmail.com>
2025-05-20 22:29:52 +0545

    feat: Add emoji picker and enhance chat functionality
    - Added emoji-picker-react to package.json for emoji support in chat.
    - Updated ProtectedRoute to fetch all users and chats on user login.
    - Implemented Home page layout with UserSearch, UsersList, and ChatArea components.
    - Created ChatArea component to handle message sending, receiving, and display.
    - Developed UserSearch component for searching users and chats.
    - Built UsersList component to display online users and chat options.
    - Enhanced message handling with typing indicators and unread message management.

--- frontend/PulseChat-frontend/src/pages/home/index.jsx
+++ frontend/PulseChat-frontend/src/pages/home/index.jsx
@@ -1,5 +1,42 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import ChatArea from "./components/ChatArea";
+import UserSearch from "./components/UserSearch";
+import UsersList from "./components/UsersList";
+
 function Home() {
-	return <div>Home</div>;
+	const [searchKey, setSearchKey] = React.useState("");
+	const { selectedChat, user } = useSelector((state) => state.userReducer);
+	const [onlineUsers, setOnlineUsers] = React.useState([]);
+	return (
+		<div className="flex gap-5">
+			{/* 1st part   user search , userslist/chatlist */}
+			<div className="w-96">
+				<UserSearch searchKey={searchKey} setSearchKey={setSearchKey} />
+				<UsersList searchKey={searchKey} onlineUsers={onlineUsers} />
+			</div>
+
+			{/* 2nd part   chatbox */}
+			{selectedChat && (
+				<div className="w-full">
+					<ChatArea />
+				</div>
+			)}
+
+			{!selectedChat && (
+				<div className="w-full h-[80vh]  items-center justify-center flex bg-white flex-col">
+					<img
+						src="https://www.pngmart.com/files/16/Speech-Chat-Icon-Transparent-PNG.png"
+						alt=""
+						className="w-96 h-96"
+					/>
+					<h1 className="text-2xl font-semibold text-gray-500">
+						Select a user to chat
+					</h1>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Home;
